e514aa436cb86ef60f4686a7b2fffde5dadb4776
Nikita <titata1@mail.ru>
2021-03-14 16:36:48 +0300

    feat: add func for brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  str=str.split("");
+  stack=[];
+  for (i=0; i<str.length; i++){
+    for (j=0;j<bracketsConfig.length;j++){
+      if (str[i]==bracketsConfig[j][0]&&bracketsConfig[j][0]==bracketsConfig[j][1]&&stack[stack.length-1]==bracketsConfig[j][0]){stack.pop(); j=bracketsConfig.length}
+      else if (str[i]==bracketsConfig[j][0]) {stack.push(str[i]); j=bracketsConfig.length}
+      else if (str[i]==bracketsConfig[j][1] && stack[stack.length-1]==bracketsConfig[j][0]) {stack.pop(); j=bracketsConfig.length}
+    }
+  }
+  return (stack.length == 0);
 }
